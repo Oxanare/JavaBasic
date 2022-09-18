199de20b36b0d6dcbf9527d0d198bc737ef43119
Oxanare <112711811+Oxanare@users.noreply.github.com>
2022-09-18 06:42:21 +0200

    Add files via upload

---
+++ JavaBasic_Homework1.java
@@ -0,0 +1,52 @@
+/**
+ * Java Basic. Homework Nr.1
+ *
+ * @author Oxana 
+ * @todo 05.09.2022
+ * @date 18.09.2022
+ */
+class JavaBasic_Homework1 {
+    public static void main(String[] args) {
+        // task 1
+        int a = 6;
+        int b = 4;
+        int p = (a + b) * 2;
+        System.out.println("Perimeter = " + p);
+        int s = a * b;
+        System.out.println("Area = " + s);
+        
+        // task 2
+        int c = 5;
+        int d = 3;
+        int e = c + d;
+        System.out.println(e);
+        int f = c - d;
+        System.out.println(f);
+        int g = c * d;
+        System.out.println(g);
+        float h = 5 / 3f;
+        System.out.println(h);
+        
+        // task 3
+        int[] array = {6, 4, 5, 3, 8};
+        System.out.printf("[%d, %d, %d, %d, %d]\n", array[0], array[1], array[2], array[3], array[4]);
+        
+        
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+    }
+}
